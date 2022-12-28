370153ebd2f48175dca5bd22c61fc2d3387174aa
Brayan de Krom <brayan.dekromhernandez@ap.be>
2022-12-28 22:02:40 +0100

    contact page finished

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,9 +1,25 @@
 import { Fragment, useRef, useState, } from "react"
-import style from "./Contact.module.css"
+import styles from "./Contact.module.css"
 import emailjs from 'emailjs-com'
 
 const Contact = () => {
-    const [name, setName] = useState("")
+    const [name, setName] = useState<string>("")
+    const [email, setEmail] = useState<string>("")
+    const [message, setMessage] = useState<string>("")
+
+    const validator = (name:string, email:string, message:string): boolean => {
+        if (name !== "" && email !== "" && message !== "") {
+            return true
+        }else{
+            return false;}
+    } 
+
+    const handleClick = () => {
+        setName("")
+        setEmail("")
+        setMessage("")
+    }
+
 
     const form = useRef();
 
@@ -19,14 +35,14 @@ const Contact = () => {
     };
 
     return (
-        <form onSubmit={sendEmail}>
+        <form className={styles.formStyle} onSubmit={sendEmail}>
             <label>Name</label>
-            <input type="text" name="name" />
+            <input type="text" name="name" onChange={event => setName(event.target.value)} value={name} placeholder="John Doe" />
             <label>Email</label>
-            <input type="email" name="email" />
+            <input type="email" name="email" onChange={event => setEmail(event.target.value)} value={email} placeholder="example@ap.be" />
             <label>Message</label>
-            <textarea name="message" />
-            <input type="submit" value="Send" />
+            <textarea name="message" onChange={event => setMessage(event.target.value)} value={message} placeholder="Write here your message"/>
+            {validator(email,name, message) && <input className={styles.submitButton} onClick={() => setTimeout(handleClick,500) } type="submit" value="Send"/>} 
         </form>
 
     )
