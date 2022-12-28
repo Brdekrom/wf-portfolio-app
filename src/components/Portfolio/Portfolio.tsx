b155eb4c0b551e2d5bbe152dcf535aa5f3e5d801
Brayan de Krom <brayan.dekromhernandez@ap.be>
2022-12-29 00:15:03 +0100

    portfollio development started

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,7 +1,98 @@
-
+import { NavLink } from "react-router-dom"
+import styles from "./Portfolio.module.css"
+import ShoppingList from "./ShoppingList/SoppingList"
 const Portfolio = () => {
-    return(
-        <></>
+    return (
+        <><main className={styles.portfolioMain}>
+            <section className={styles.bouncy4}>
+                <div>
+                    <header>
+                    <h1>ShoppingList</h1>
+                    </header>
+                    <body>
+                        <p>Here you will be able to check a little shopping list created with React, typescript and no further non-react libraries</p>
+                    </body>
+                    <button>
+                            <NavLink to="/portfolio/shoppinglist">Check it out!</NavLink>
+                        </button>
+                </div>
+            </section>
+
+            <section className={styles.bouncy3}>
+                <div>Hello world
+                    <header>
+                        <button>
+                            <NavLink to="/">Or this one?</NavLink>
+                        </button>
+                    </header>
+                </div>
+            </section>
+
+            <section className={styles.bouncy2}>
+                <div>Hello world
+                    <header>
+                        <button>
+                            <NavLink to="/">Try this one!</NavLink>
+                        </button>
+                    </header>
+                </div>
+            </section>
+
+            <section className={styles.bouncy1}>
+                <div>Hello world
+                    <header>
+                        <button>
+                            <NavLink to="/">Or this one?</NavLink>
+                        </button>
+                    </header>
+                </div>
+            </section>
+
+            <section className={styles.bouncy1}>
+                <div>Hello world<header><button>
+                    <NavLink to="/">Look to this one!</NavLink>
+                </button>
+                </header>
+                </div>
+            </section>
+
+            <section className={styles.bouncy2}>
+                <div>Hello world
+                    <header>
+                        <button>
+                            <NavLink to="/">How about this one?</NavLink>
+                        </button>
+                    </header>
+                </div>
+            </section>
+            
+            <section className={styles.bouncy3}>
+                <div>
+                    Hello world
+                    <header>
+                        <button>
+                            <NavLink to="/">Check this one out!!</NavLink>
+                        </button>
+                    </header>
+                </div>
+            </section>
+            
+            <section className={styles.bouncy4}>
+                <div>Hello world
+                    <header>
+                        <button>
+                            <NavLink to="/">Maybe this one then?</NavLink>
+                        </button>
+                    </header>
+                </div>
+            </section>
+
+
+
+
+        </main>
+        </>
+
     )
 }
 
---
+++ src/components/Portfolio/ShoppingList/SoppingList.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import styles from "./ShoppingList.module.css"
+
+interface ShoppingListItem {
+  name: string;
+  quantity: number;
+}
+
+const ShoppingList = () => {
+  const [ShoppingList, setShoppinglist] = useState<ShoppingListItem[]>([{
+    name: "aardappelen",
+    quantity: 5
+  }])
+  const [name, setName] = useState<string>("")
+  const [quantity, setQuantity] = useState<number>(0)
+  const [successMessage, setSuccesMessage] = useState<string>("")
+  const [errorsMessage, setErrorMessage] = useState<string>("")
+
+  const addShoppingItem = () => {
+    if (quantity < 1) {
+      setErrorMessage("Quantity should be above 0")
+      setSuccesMessage("")
+    }
+    else {
+      setShoppinglist([...ShoppingList, { name: name, quantity: quantity }]);
+      setSuccesMessage("Item added to shopping list");
+      setErrorMessage("");
+    }
+  }
+
+  const removeShoppingItem = (index: number) => {
+    let shoppingListCopy = ShoppingList.filter((item, i) => i !== index)
+    setShoppinglist(shoppingListCopy)
+    setSuccesMessage("Item removed from shopping list");
+    setErrorMessage("")
+  }
+
+
+  return (
+    <div className={styles.shoppingAddForm}>
+      <div>
+        {errorsMessage && <div className={styles.error}>
+          {errorsMessage}
+        </div>}
+        {successMessage && <div className={styles.success}>
+          {successMessage}
+        </div>}
+
+        <label>Name:</label>
+        <input type="text" placeholder='name' value={name} onChange={(event) => setName(event.target.value)}></input>
+        <label>Quantity:</label>
+        <input type="number" placeholder="Quantity" value={quantity} onChange={(event) => setQuantity(parseInt(event.target.value))} />
+      </div>
+      <button onClick={addShoppingItem}>Add</button>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Quantity</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+        {
+          ShoppingList.map((item, index) => (
+            <tr>
+              <td>{item.name}</td>
+              <td>{item.quantity}</td>
+              <td>
+                <button onClick={() => { removeShoppingItem(index) }}>Remove</button>
+              </td>
+            </tr>
+          ))
+        }
+        </tbody>
+      </table>
+    </div>
+
+  )
+}
+
+export default ShoppingList
