de80f41e8ff4cf57a15cee9fff167666a381ce31
Brayan de Krom <brayan.dekromhernandez@ap.be>
2022-12-28 17:40:11 +0100

    profile pic resized

---
+++ src/components/Home/HomeImg.tsx
@@ -0,0 +1,10 @@
+import styles from "./Home.module.css"
+import { NavLink } from "react-router-dom"
+import HomeButton from "./HomeButton";
+
+
+const HomeImg = async () => {
+    <figure>
+        <img src="/bdk.jpg" alt="" className={styles.bdk} />
+    </figure>
+}
