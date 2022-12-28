a386b161ada097e4c9bfd2c23e592d7b7a595964
Brayan de Krom <brayan.dekromhernandez@ap.be>
2022-12-28 19:15:22 +0100

    contact form development started

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,27 +1,35 @@
-import { Fragment, useState,  } from "react"
+import { Fragment, useRef, useState, } from "react"
 import style from "./Contact.module.css"
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
-    const [name, setName] = useState<string>("")
-    const [email, setEmail] = useState<string>("")
+    const [name, setName] = useState("")
 
-    const successMessage = () => {
+    const form = useRef();
 
-        return (
-            <Fragment>
-                <p>Hi {name}, Your email was sent succesfully, We'll reach you out on your email: {email}</p>
-                <p>Until then!</p>
-            </Fragment>
+    const sendEmail = (e:any) => {
+        e.preventDefault();
 
-        )
-    }
+        emailjs.sendForm('default_service', 'default_template', e.target, 'OTP85_cjHYuhqr02-')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
 
     return (
-        <form action="">
-            lab
+        <form onSubmit={sendEmail}>
+            <label>Name</label>
+            <input type="text" name="name" />
+            <label>Email</label>
+            <input type="email" name="email" />
+            <label>Message</label>
+            <textarea name="message" />
+            <input type="submit" value="Send" />
         </form>
 
-)
+    )
 
 }
 
