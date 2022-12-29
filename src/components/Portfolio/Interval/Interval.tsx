3b2ebcc1d79eba011865f713411b2938e7bd67e8
Brayan de Krom <brayan.dekromhernandez@ap.be>
2022-12-29 19:22:46 +0100

    Finished interval component

--- src/components/Portfolio/Interval/Interval.tsx
+++ src/components/Portfolio/Interval/Interval.tsx
@@ -1,6 +1,7 @@
 import Timer from "./Timer"
 import styles from "./Interval.module.css"
 import CurrentTime from "./CurrentTime"
+import RandomValue from "./RandomValue"
 
 
 const Interval = () => {
@@ -11,6 +12,10 @@ const Interval = () => {
         <Timer/>
         <br />
         <CurrentTime/>
+        <br />
+        <RandomValue min={1} max={100}/>
+        <br />
+        <RandomValue min={100} max={200}/>
         </div>
         
         </>
--- src/components/Portfolio/Interval/RandomValue.tsx
+++ src/components/Portfolio/Interval/RandomValue.tsx
@@ -1,14 +1,30 @@
 import { useEffect, useState } from "react"
+
+
+const getRandomValue = (min:number, max: number) => {
+    return Math.floor(Math.random() * (max - min)) + min
+}
+
 interface RandomValueProps {
     min:number,
     max:number
 }
 
 const RandomValue = ({min, max}:RandomValueProps) => {
+    const [randomValue, setRandomValue] = useState(getRandomValue(min, max))
+
+    useEffect(() => {
+       let handle = setInterval(()=> {
+        setRandomValue(getRandomValue(min, max))
+       }, 2000) 
+
+       return () => {clearInterval(handle)}
+    },[])
+
     return(
-        <p>
-            Random value between {min} and {max}: 
-        </p>
+        <>
+            Random value between {min} and {max}: {randomValue}
+        </>
     )
 }
 
