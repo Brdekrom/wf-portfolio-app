e8ef2fe116386e1490aa4b3702d40e81ca665b8f
Brayan de Krom <brayan.dekromhernandez@ap.be>
2023-01-19 17:59:19 +0100

    Finished QuizApp component

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -12,30 +12,37 @@ const Portfolio = () => {
            title="ShoppingList"
            text="Here you will be able to check a little shopping list created with React, typescript and no further non-react libraries."
            navLinkTo="/portfolio/shoppinglist"/>
+
            <PortfolioModule
            title="Filtering"
            text="Small filter and sort demonstration. Warning: Caps sensitive."
            navLinkTo="/portfolio/filtering"/>
+
            <PortfolioModule
            title="Interval"
            text="Showing a timer, current time and random values."
            navLinkTo="/portfolio/interval"/>
+
            <PortfolioModule
            title="Pokedex"
            text="Pokemon app using API calls from https://pokeapi.co/."
            navLinkTo="/portfolio/interval"/>
+
            <PortfolioModule
            title="DadJoke"
            text="DadJokes with a favorite joke setter using API calls from https://icanhazdadjoke.com/."
            navLinkTo="/portfolio/dadjoke"/>
+
            <PortfolioModule
            title="Todo's"
            text="Small todo's list with checkboxes."
            navLinkTo="/portfolio/todo"/>
+
            <PortfolioModule
            title="QuizApp"
            text="Small QuizApp with 10 questions."
            navLinkTo="/portfolio/quizapp"/>
+
            <PortfolioModule
            title="To be finished"
            text="N/A"
@@ -45,9 +52,6 @@ const Portfolio = () => {
                 <p>Please feel free to contact Me on the button here below: </p>
                 <HomeButton linkTo={"/contact"} text={"Contact"}></HomeButton>
             </section>
-
-
-
         </main>
         </>
 
--- src/components/Portfolio/QuizApp/MultipleChoiceQuestion.tsx
+++ src/components/Portfolio/QuizApp/MultipleChoiceQuestion.tsx
@@ -1,31 +1,37 @@
-import Question from "./Question"
 import { QuizQuestion } from "./types"
 
-interface MultipleChoiseQuestionProps {
-    question: QuizQuestion
-    setUserAnswer: (answer: string) => void
+interface MultipleChoiceQuestionProps {
+    question: QuizQuestion,
+    setUserAnswer: (answer:string)=>void
 }
 
-const sortAnswers = (correctAnswer: string, wrongAnswers: string[]) => {
-    let allAnswers = [correctAnswer, ...wrongAnswers]
-    allAnswers.sort((a,b) => Math.random() - 0.5)
-    return allAnswers
+interface sortAnswersProps {
+    correctAnswer: string,
+    wrongAnswers: string[]
 }
 
-const MultipleChoiceQuestion  = ({question, setUserAnswer}: MultipleChoiseQuestionProps) => {
+const sortAnswers = (toSortAnswers: sortAnswersProps) => {
+    const { correctAnswer, wrongAnswers } = toSortAnswers
+    let sortedAnswers = [correctAnswer, ...wrongAnswers]
+    sortedAnswers.sort((a, b) => Math.random() - 0.5)
+    return sortedAnswers
+}
+
+
+const MultipleChoiceQuestion = ({question, setUserAnswer} : MultipleChoiceQuestionProps) => {
     let allQuestions = [...question.incorrect_answers, question.correct_answer]
-    allQuestions.sort((a,b) => Math.random() - 0.5)
-    return ( 
+    allQuestions.sort((a,b) => Math.random()-0.5)
+
+    return (
         <div>
-            <select onChange={event => setUserAnswer(event.target.value)} >
-                <option value=""></option>
+            <select onChange={e => setUserAnswer(e.target.value)}>
+                <option>Select an Answer</option>
                 {allQuestions.map((answer, index) => {
-                    return <option key={index} value={answer}>{answer}</option>
+                    return <option key={index} value={answer}>{answer}</option> 
                 }) }
             </select>
         </div>
-
     )
 }
 
-export default MultipleChoiceQuestion 
+export default MultipleChoiceQuestion
